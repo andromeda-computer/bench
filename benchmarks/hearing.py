3a91a2bb7f3f4cd5bebe460a72249c85dce6de61
CJ Pais <cj@cjpais.com>
2024-06-08 17:24:49 -0700

    proper calculation for speedup

--- benchmarks/hearing.py
+++ benchmarks/hearing.py
@@ -6,6 +6,6 @@ class HearingBenchmarkResult():
     def __init__(self, json, time, power):
         self.text = json['text']
         self.input_seconds = json['duration']
-        self.transcribe_time = json['transcribe_time']
+        self.transcribe_time = json['transcribe_time'] / 1000
         self.speedup = self.input_seconds / self.transcribe_time
         self.speedup_watt = self.speedup / power[2]
